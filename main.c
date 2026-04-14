4d8c8a34516eae4e1214107d670e41e0b2a35b4d
wagersre <wagersre@gmail.com>
2026-04-14 16:39:08 -0400

    USB-C Communication Confirmed
    Using new USB-C cords, communication has been established. Return code to its MENU selection process.

--- main.c
+++ main.c
@@ -62,10 +62,7 @@ int main()
 
     while (1)
     {
-        printf("Test USBC for Communicaion\n");
-        sleep_ms(1000);
 
-        /*
         // PHASE 1: UI SETUP DISPATCHER
         // Runs until current_screen == InitializationDone
         while (current_screen != InitializationDone)
@@ -73,7 +70,7 @@ int main()
             /*
             uart_command_received = 0;
             ParseRCI();
-            
+            */
             if (force_redraw)
             {
                 switch (current_screen)
@@ -107,7 +104,6 @@ int main()
             }
             WaitForInput(); // Now detects the Soft Return flag
         }
-        */
             
     }
     
