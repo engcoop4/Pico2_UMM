64d134a7e48190f19bdc327e8483aadb6d066d0c
wagersre <wagersre@gmail.com>
2026-07-01 12:53:06 -0400

    Addition of Files for I2C GPIO Extension Preparation
    No actual code written yet for I2C GPIO Extension, but files created in preparation for it.

---
+++ I2CExtension.c
@@ -0,0 +1,8 @@
+/**
+ * @file I2CExtension.c
+ * @author engcoop#4 RW
+ * @brief Contains initialization and functions in regards to the GPIO I2C Extenders used for the LEDs, buttons, and blade board identification.
+ * @version 1.0.0
+ * @date 2026-07-01
+ * @copyright Copyright (c) 2026
+ */
---
+++ I2CExtension.h
@@ -0,0 +1,8 @@
+/**
+ * @file I2CExtension.h
+ * @author engcoop#4 RW
+ * @brief Header file for I2CExtension.c.
+ * @version 1.0.0
+ * @date 2026-07-01
+ * @copyright Copyright (c) 2026
+ */
